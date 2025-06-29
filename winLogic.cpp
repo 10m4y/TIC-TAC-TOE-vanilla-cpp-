1522fd414fef58b8e339038eb8eef38e22bf28a7
cypher4802 <tanmay258789@gmail.com>
2025-06-30 01:02:53 +0530

    added win logic

--- winLogic.cpp
+++ winLogic.cpp
@@ -2,6 +2,34 @@
 using namespace std;
 
 
-int checkWin(vector<vector<int>>&grid){
-    
+bool checkWin(vector<vector<int>>&grid){
+    if(grid[0][0]==grid[1][1] && grid[1][1]==grid[2][2] && grid[2][2]!=-1){
+        return true;
+    }
+    else if(grid[0][2]==grid[1][1] && grid[1][1]==grid[2][0]&&grid[2][0]!=-1){
+        return true;
+    }
+    else if(grid[0][0]==grid[0][1] && grid[0][1]==grid[0][2]&&grid[0][0]!=-1){
+        return true;
+    }
+    else if(grid[1][0]==grid[1][1] && grid[1][1]==grid[1][2]&&grid[1][0]!=-1){
+        return true;
+    }
+    else if(grid[2][0]==grid[2][1] && grid[2][1]==grid[2][2]&&grid[2][0]!=-1){
+        return true;
+    }
+    else if(grid[0][0]==grid[1][0] && grid[1][0]==grid[2][0]&&grid[0][0]!=-1){
+        return true;
+    }
+    else if(grid[0][1]==grid[1][1] && grid[1][1]==grid[2][1]&&grid[0][1]!=-1){
+        return true;
+    }
+    else if(grid[0][2]==grid[1][2] && grid[1][2]==grid[2][2]&&grid[0][2]!=-1){
+        return true;
+    }
+    else{
+        return false;
+    }
+
+
 }
